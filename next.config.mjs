9f644c054fecee0c12c0f8045516f920a049fe27
MrFluffycloud <aliadraseen@gmail.com>
2025-10-07 01:54:23 +0530

    New Changes - 10-7-2025

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+
+	basePath: '/TedX-Farook-College.github.io',
+};
 
 export default nextConfig;
