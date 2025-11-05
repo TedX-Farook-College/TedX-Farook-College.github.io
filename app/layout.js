e6a1a41e81cbb73395b96fbf3c19ec9cd3ec1054
MrFluffycloud <aliadraseen@gmail.com>
2025-11-06 00:05:12 +0530

    idk tbh about this one

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export const metadata = {
 		siteName: 'TEDx Farook College',
 		images: [
 			{
-				url: '/og-image.jpg', // need to add our image
+				url: '/og-image.jpg',
 				width: 1200,
 				height: 630,
 				alt: 'TEDx Farook College',
@@ -61,7 +61,7 @@ export const metadata = {
 		apple: '/apple-touch-icon.png', // need to add apple touch icon
 		other: {
 			rel: 'apple-touch-icon-precomposed', // here too
-			url: '/apple-touch-icon-precomposed.png',
+			url: '/apple-touch-icon.png',
 		},
 	},
 	manifest: '/site.webmanifest',
@@ -74,6 +74,10 @@ export const viewport = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<meta name="apple-mobile-web-app-title" content="TEDxFC" />
+			</head>
+
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
